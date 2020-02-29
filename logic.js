788dc2a5174eaaa824d84aaa4879907f13c18c45
Jacob Zirbel <jzerble@gmail.com>
2020-02-29 16:02:57 -0600

    make basic portfolio responsive

---
+++ logic.js
@@ -0,0 +1,8 @@
+function submit() {
+  alert("submit");
+}
+
+function showContent(s) {
+  document.getElementById("project1-content").style = "display=block";
+  window.scrollTo(0, document.body.scrollHeight);
+}
